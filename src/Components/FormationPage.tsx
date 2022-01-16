96b1ea35a65dbd69e8b8ac9e3ba3319b80eb68b9
Amine <amineboussoualim@gmail.com>
2022-01-16 14:56:40 +0100

    added new Lemot project

--- src/Components/FormationPage.tsx
+++ src/Components/FormationPage.tsx
@@ -11,7 +11,7 @@ function FormationPage() {
                 <p className="text-neon-blue text-5xl mb-6 mt-6 lg:mt-0">Experiences</p>
                 <div className="overflow-hidden flex flex-col lg:flex-row lg:justify-evenly items-center w-full">
                     {data.experiences.map((e, i) =>
-                        <div className="flex flex-col items-center text-center border-4 bg-neon-yellow/10 border-neon-yellow relative lg:w-1/4 h-72 w-4/5 my-8 lg:my-0">
+                        <div key={i} className="flex flex-col items-center text-center border-4 bg-neon-yellow/10 border-neon-yellow relative lg:w-1/4 h-72 w-4/5 my-8 lg:my-0">
                             <BorderEdgeCut />
                             <p className="mt-8 mx-2 text-neon-yellow text-2xl">{e.titre}</p>
                             <p className="text-xl">{e.years}</p>
@@ -25,7 +25,7 @@ function FormationPage() {
                 <p className="text-neon-blue text-5xl mb-6">Formations</p>
                 <div className="overflow-hidden flex flex-col lg:flex-row lg:justify-evenly items-center w-full">
                     {data.formations.map((f, i) =>
-                        <div className="flex flex-col items-center text-center border-4 bg-neon-yellow/10 border-neon-yellow relative lg:w-1/4 h-72  w-4/5 my-8 lg:my-0">
+                        <div key={i} className="flex flex-col items-center text-center border-4 bg-neon-yellow/10 border-neon-yellow relative lg:w-1/4 h-72  w-4/5 my-8 lg:my-0">
                             <BorderEdgeCut />
                             <p className="mt-8 mx-2 text-neon-yellow text-2xl">{f.titre}</p>
                             <p className="text-sm xl:text-xl">{f.lieu}</p>
--- src/Components/ProjectsPage.tsx
+++ src/Components/ProjectsPage.tsx
@@ -1,12 +1,13 @@
 import BorderEdgeCut from "./BorderEdgeCut"
 
 import Projetf1pic from "../assets/projetf1thumb.png"
-import ProjetPortfolioPick from "../assets/screenPortfolio.png"
+import ProjetPortfolioPic from "../assets/screenPortfolio.png"
+import ProjetLemotPic from "../assets/screenLemot.png"
 import data from "../assets/data.json"
 import { useState } from "react"
 
 function ProjectsPage() {
-    const projectPictures = [Projetf1pic, ProjetPortfolioPick]
+    const projectPictures = [Projetf1pic, ProjetPortfolioPic, ProjetLemotPic]
     const nbProjects = data.projets.length - 1;
     const [selectedTab, setSelectedTab] = useState(0)
 
@@ -17,7 +18,7 @@ function ProjectsPage() {
                 <BorderEdgeCut />
                 <div className="flex flex-col lg:flex-row items-center text-center lg:text-sm text-xs h-full lg:h-16 w-16 lg:w-full bg-neon-yellow/20 ">
                     {data.projets.map(({ onglet }, i) =>
-                        <div onClick={() => i < nbProjects && setSelectedTab(i)} className={`flex items-center justify-center lg:h-full lg:w-1/6 h-1/6 w-full p-2 ${i === selectedTab ? 'bg-neon-yellow/50' : 'bg-neon-yellow/30'} 
+                        <div key={i} onClick={() => i < nbProjects && setSelectedTab(i)} className={`flex items-center justify-center lg:h-full lg:w-1/6 h-1/6 w-full p-2 ${i === selectedTab ? 'bg-neon-yellow/50' : 'bg-neon-yellow/30'} 
                          ${i < nbProjects ? "hover:bg-neon-yellow/50 hover:cursor-pointer" : "hover:cursor-not-allowed "}`}>
                             <p className="">{onglet}</p>
                         </div>
@@ -28,8 +29,9 @@ function ProjectsPage() {
                     <div className="lg:flex px-5 justify-center items-center hidden w-1/2">
                         <a href={data.projets[selectedTab].lien} target="_blank" rel="noopener noreferrer"><img className="rounded-lg" src={projectPictures[selectedTab]} alt="Amine"></img></a>
                     </div>
-                    <div className="flex text-center text-xl lg:w-1/2">
+                    <div className="flex flex-col text-center text-xl lg:w-1/2">
                         <p className="m-2 text-sm lg:text-xl">{data.projets[selectedTab].desc}</p>
+                        <p>Site : <a href={data.projets[selectedTab].lien} target="_blank" rel="noopener noreferrer" className="underline">{data.projets[selectedTab].onglet}</a></p>
                     </div>
                 </div>
 
